d69d7fedd0930d49d5ddbb61908d32837237d34a
Hamida REBAI <didourebai@gmail.com>
2022-07-28 16:44:37 -0400

    Ajouter chapitre 4 Fin

---
+++ CHAPITRE4_VIDEO04_03_f/EvaluationProduit/EvaluationProduit.MVC/Models/ProduitModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EvaluationProduit.MVC.Models
+{
+    public class ProduitModel : ModeleBase
+    {
+        public int MoyenneEvaluation { get; set; }
+        public decimal Prix { get; set; }
+        public CategorieModel CategorieModel { get; set; }
+
+    }
+}
