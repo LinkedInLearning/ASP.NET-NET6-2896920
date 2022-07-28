f6353d707f4cf915e75ce1eb983f2f621b8fa272
Hamida REBAI <didourebai@gmail.com>
2022-07-28 16:19:17 -0400

    Ajouter CHAPITRE1

---
+++ ExempleMVCDemo/ExempleMVCDemo/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+     name: "default",
+     pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
