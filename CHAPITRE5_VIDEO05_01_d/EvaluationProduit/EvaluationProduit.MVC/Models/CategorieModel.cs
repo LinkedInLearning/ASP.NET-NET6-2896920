18fb562eaad09549c29f90a48a8d818791d39e1f
Hamida REBAI <didourebai@gmail.com>
2022-07-28 16:45:48 -0400

    Ajouter chapitre 5

---
+++ CHAPITRE5_VIDEO05_01_d/EvaluationProduit/EvaluationProduit.MVC/Models/CategorieModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EvaluationProduit.MVC.Models
+{
+    public class CategorieModel
+    {
+        public int CategorieId { get; set; }
+        public string NomCategorie { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ CHAPITRE5_VIDEO05_01_d/EvaluationProduit/EvaluationProduit.MVC/Services/ICategorieService.cs
@@ -0,0 +1,10 @@
+﻿using EvaluationProduit.MVC.Models;
+
+namespace EvaluationProduit.MVC.Services
+{
+    public interface ICategorieService
+    {
+        IList<CategorieModel> CategorieModels { get; set; }
+        IList<string> CategorieList { get; set; }
+    }
+}
