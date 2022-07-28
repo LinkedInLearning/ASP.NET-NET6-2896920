fecad9ede6a7e40941bc22a765a7ca296f600349
Hamida REBAI <didourebai@gmail.com>
2022-07-28 16:34:59 -0400

    Ajouter chapitre 2

---
+++ CHAPITRE2_VIDEO02_01_d/SystemTextJsonExemple/SystemTextJsonExemple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Categorie framework = new()
+{
+    Name = ".NET 6",
+};
+Categorie systemTextJson = new()
+{
+    Name = "System.Text.Json",
+    Parent = framework
+};
+framework.Enfants.Add(systemTextJson);
+
+JsonSerializerOptions options = new()
+{
+    ReferenceHandler = ReferenceHandler.IgnoreCycles,
+    WriteIndented = true
+};
+
+string dotnetJson = JsonSerializer.Serialize(framework, options);
+Console.WriteLine($"{dotnetJson}");
+
+public class Categorie
+{
+    public string Name { get; set; }
+    public Categorie Parent { get; set; }
+    public List<Categorie> Enfants { get; set; } = new();
+}
